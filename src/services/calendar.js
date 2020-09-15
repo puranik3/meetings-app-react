3ad90136a11c77be36edd6b5d3d574dcd406f9bc
Prashanth Puranik <puranik@digdeeper.in>
2020-09-15 15:25:52 +0530

    minor

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 function getCalendar() {
-    // - For API call...
+    // - For API call, use axios...
     // - if you have implemented authentication - make a call to /api/auth/login in Postman and get a token for a user and harcode 'Authorization': 'the token you got from Postman' when making a call to /api/calendar
     // - if you have not implemented authentication - add ?email=<useremail> (or whatever query params represent the user in your calendar API) when making a call to /api/calendar
 
